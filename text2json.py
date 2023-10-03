074c7154e9809a946e9d806bad517c078470c720
‘Xiao <xiao.liu.contact@gmail.com>
2023-10-03 19:55:09 +1000

    update notebook and dataset

--- text2json.py
+++ text2json.py
@@ -12,7 +12,7 @@ def parse_line(line):
         return None
 
 # Read the text file
-with open('Dataset.txt', 'r') as file:
+with open('/content/SearchQuery2FuncCall/Dataset.txt', 'r') as file:
     lines = file.readlines()
 
 # Parse the lines into input-output pairs
@@ -26,3 +26,26 @@ with open('flan_t5_q2f_dataset.json', 'w') as json_file:
     json.dump(data, json_file, indent=2)
 
 print(f"Saved {len(data)} examples to 'flan_t5_q2f_dataset.json'.")
+
+# Initialize two empty lists for the separate datasets
+search_examples = []
+non_search_examples = []
+
+# Iterate through the dataset and separate examples
+for example in data:
+    if example['output'] == "Search()":
+        example['Search']=1
+        search_examples.append(example)
+    else:
+        example['Search']=0
+        non_search_examples.append(example)
+
+# Save the separated datasets as JSON files
+with open('search_examples.json', 'w') as json_file:
+    json.dump(search_examples, json_file, indent=2)
+
+with open('non_search_examples.json', 'w') as json_file:
+    json.dump(non_search_examples, json_file, indent=2)
+
+print(f"Separated {len(search_examples)} Search() examples to 'search_examples.json'.")
+print(f"Separated {len(non_search_examples)} non-Search() examples to 'non_search_examples.json'.")
