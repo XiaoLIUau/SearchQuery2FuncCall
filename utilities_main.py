1dbc3ef415041b3a131b79202476244551c55e0c
‘Xiao <xiao.liu.contact@gmail.com>
2023-10-16 19:14:28 +1000

    update login huggingface method

--- utilities_main.py
+++ utilities_main.py
@@ -15,31 +15,10 @@ from datasets import DatasetDict, Dataset
 from trl import SFTTrainer
 
 
-#####################
-### Load datasets ###
-#####################
-text2json('/content/SearchQuery2FuncCall/Dataset.txt')
-# q2f_datasets = load_n_process_data('/content/non_search_examples.json')
-q2f_datasets = load_n_process_data('/content/q2f_dataset.json')
-print(q2f_datasets)
 
-
-# # Load Huggingface API key
-# Here we use Huggingface models
-# Note: Please load a text file that contains your model api key to current folder
-# >Name your file in either ***'api_key_huggingface.txt'***
-
-### Access Huggingface for loading model
-""" # Get model api key """
-def load_api_key_from_file(file_path):
-  with open(file_path, 'r') as file:
-      api_key = file.read().strip()
-  return api_key
-# Setting a new environment variable
-os.environ["HUGGINGFACE_TOKEN"] = load_api_key_from_file('/content/api_key_huggingface.txt')
-!huggingface-cli login --token $HUGGINGFACE_TOKEN
-
-
+########################################
+### Setup for Check Model Parameters ###
+########################################
 ### Funciton to print number of trainable mode parameters
 def print_number_of_trainable_model_parameters(model):
   trainable_model_params = 0
