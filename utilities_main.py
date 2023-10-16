69abd3d9791b36cd02af1df5a11097900ec6a325
‘Xiao <xiao.liu.contact@gmail.com>
2023-10-16 18:51:54 +1000

    Create utilities files with setup funcs for reuse

---
+++ utilities_main.py
@@ -0,0 +1,187 @@
+########################
+### Import libraries ###
+########################
+import os
+import time
+import pandas as pd
+import torch
+import evaluate
+import tqdm
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, GenerationConfig, TrainingArguments, Trainer
+from peft import PeftModel, LoraConfig, get_peft_model, TaskType
+from SearchQuery2FuncCall.setup_dataset import text2json, load_n_process_data
+from datasets import DatasetDict, Dataset
+from trl import SFTTrainer
+
+
+#####################
+### Load datasets ###
+#####################
+text2json('/content/SearchQuery2FuncCall/Dataset.txt')
+# q2f_datasets = load_n_process_data('/content/non_search_examples.json')
+q2f_datasets = load_n_process_data('/content/q2f_dataset.json')
+print(q2f_datasets)
+
+
+# # Load Huggingface API key
+# Here we use Huggingface models
+# Note: Please load a text file that contains your model api key to current folder
+# >Name your file in either ***'api_key_huggingface.txt'***
+
+### Access Huggingface for loading model
+""" # Get model api key """
+def load_api_key_from_file(file_path):
+  with open(file_path, 'r') as file:
+      api_key = file.read().strip()
+  return api_key
+# Setting a new environment variable
+os.environ["HUGGINGFACE_TOKEN"] = load_api_key_from_file('/content/api_key_huggingface.txt')
+!huggingface-cli login --token $HUGGINGFACE_TOKEN
+
+
+### Funciton to print number of trainable mode parameters
+def print_number_of_trainable_model_parameters(model):
+  trainable_model_params = 0
+  all_model_params = 0
+  for _, param in model.named_parameters():
+      all_model_params += param.numel()
+      if param.requires_grad:
+          trainable_model_params += param.numel()
+  return f"trainable model parameters: {trainable_model_params}\nall model parameters: {all_model_params}\npercentage of trainable model parameters: {100 * trainable_model_params / all_model_params:.2f}%"
+
+
+
+##################################
+### Setup for Model Generation ###
+##################################
+# define prompt format
+def create_prompt(input):
+  input_prompt = 'Input:'
+  output_prompt = ', Output:'
+  instruction = f"""Instruction: Given a search query, then route to different backend components based on the search intent.
+1. If the search is about unit conversion, return API function UnitConvert(SourceUnit, TargetUnit, SourceValue).
+2. If the search is about calculation, return API function Calculate(Equation).
+3. If the search is about other search intent, return API function Search().
+* For unit conversion: common unit conversion in length, mass, time, area, speed, temperature, volume should be covered. And it should be consistent for the same unit throughout. E.g. it should always be “foot”, it cannot be “feet” or “ft” in API calls.
+* For calculation: common operation such as +, -, *, /, pow, log, ln, exp, tan(h), sin(h), cos(h), factorial should be covered. And it should be consistent for the same operation throughout. E.g. it should always be “ * ”, it cannot be “x” or “X” in API calls.
+Handle input queries in different language styles. Cover common unit conversion and calculation operations.
+
+"""
+  prompt = instruction + input_prompt + f'“{input}”' + output_prompt
+  return prompt
+
+# Generate response in tokens with given model and tokenized input
+def model_generate(input,model,tokenizer):
+  generation_config = model.generation_config
+  generation_config.max_new_tokens = 100
+  generation_config.temperature = 0.00000000000001
+  generation_config.top_p = 0.9
+  generation_config.num_return_sequences = 1
+  generation_config.pad_token_id = tokenizer.eos_token_id
+  generation_config.eos_token_id = tokenizer.eos_token_id
+  return model.generate(
+          input_ids = input.input_ids,
+          attention_mask = input.attention_mask,
+          generation_config = generation_config,
+          )
+
+# Generate Text with selected model and input text
+def generated_text(input,model,tokenizer):
+  prompt = create_prompt(input)
+  inputs = tokenizer(prompt, return_tensors='pt')
+  generated = tokenizer.decode(
+      model_generate(inputs,model,tokenizer)[0],
+      skip_special_tokens=True
+  )
+  return generated
+
+# Post generation string processing
+def extractOutputString(input_string,output_string):
+  import re
+  # Use regular expressions to find the matching output for the input query
+  output_match = re.search(rf'Input:\s*“{re.escape(input_string)}”\s*,\s*Output:\s*(.*?)(\[\/|$)', output_string, flags=re.MULTILINE)
+
+  # Extract and print the output
+  if output_match:
+      output_string = output_match.group(1)
+
+  # Remove quotation marks
+  prefixes = ['“', '”', "'", '"', '[', '.', ']']
+  if output_string.startswith(tuple(prefixes)):
+      output_string = output_string[1:]
+  while output_string.endswith(tuple(prefixes)):
+      output_string = output_string[:-1]
+  # Remove all space in output
+  output_string = "".join(output_string.split())
+  return output_string
+
+
+
+############################
+### Setup for Model Test ###
+############################
+## Test model output and dataset evaluation using ROUGE and BLEU scores
+def generate_samples(example_indices,dataset,model,tokenizer):
+    dash_line = '-'.join('' for x in range(100))
+
+    for i, index in enumerate(example_indices):
+        input = dataset['test'][index]['input']
+        output = dataset['test'][index]['output']
+
+        generated = generated_text(input,model,tokenizer)
+        generated = extractOutputString(input,generated)
+
+        print(dash_line)
+        print('Example ', i + 1)
+        print(dash_line)
+        print(f'INPUT:\n{input}')
+        print(dash_line)
+        print(f'BASELINE OUTPUT:\n{output}')
+        print(dash_line)
+        print(f'PEFT MODEL GENERATION - OUTPUT:\n{generated}\n')
+    return
+
+
+def generate_dataset(start_index,dataset,model,tokenizer):
+    end_index=start_index+len(dataset['test'])
+    inputs = dataset['test'][start_index:end_index]['input']
+    outputs = dataset['test'][start_index:end_index]['output']
+
+    outputs_gen = []
+
+    for idx, input in enumerate(inputs):
+        if idx%10==0:
+            print(idx)
+        output_gen = generated_text(input,model,tokenizer)
+        output_gen = extractOutputString(input,output_gen)
+        outputs_gen.append(output_gen)
+
+    zipped_summaries = list(zip(inputs, outputs, outputs_gen))
+    df = pd.DataFrame(zipped_summaries, columns = ['Inputs', 'Outputs', 'Outputs_generated'])
+    return df
+
+
+def evaluate_generations(outputs_ref,outputs_gen):
+    # Rouge
+    rouge = evaluate.load('rouge')
+    model_results = rouge.compute(
+        predictions=outputs_gen,
+        references=outputs_ref,
+        use_aggregator=True,
+        use_stemmer=True,
+    )
+
+    print('MODEL ROUGE SCORES:')
+    print(model_results)
+
+    # bleu
+    bleu = evaluate.load('bleu')
+    model_results = bleu.compute(
+        predictions=outputs_gen,
+        references=outputs_ref,
+    )
+
+    print('MODEL BLEU SCORES:')
+    print(model_results)
+    return
---
+++ utilities_train_peft.py
@@ -0,0 +1,74 @@
+from utilities_main import print_number_of_trainable_model_parameters
+
+##########################
+### Setup for Training ###
+##########################
+
+### Prepare for training dataset
+def create_prompt_training(input,output):
+    input_prompt = 'Input:'
+    output_prompt = ', Output:'
+    start_prompt = '<s>[INST] '
+    end_prompt = '[/INST]'
+    instruction = f"""Instruction: Given a search query, then route to different backend components based on the search intent.
+1. If the search is about unit conversion, return API function UnitConvert(SourceUnit, TargetUnit, SourceValue).
+2. If the search is about calculation, return API function Calculate(Equation).
+3. If the search is about other search intent, return API function Search().
+* For unit conversion: common unit conversion in length, mass, time, area, speed, temperature, volume should be covered. And it should be consistent for the same unit throughout. E.g. it should always be “foot”, it cannot be “feet” or “ft” in API calls.
+* For calculation: common operation such as +, -, *, /, pow, log, ln, exp, tan(h), sin(h), cos(h), factorial should be covered. And it should be consistent for the same operation throughout. E.g. it should always be “ * ”, it cannot be “x” or “X” in API calls.
+Handle input queries in different language styles. Cover common unit conversion and calculation operations.
+
+"""
+
+    prompt = start_prompt + instruction + input_prompt + f'“{input}”' + output_prompt + f'“{output}”' + end_prompt
+    return prompt
+
+# Examples:
+# {input_prompt}“ft to cm”{output_prompt}“UnitConvert(SourceUnit:foot, TargetUnit:centimeter,
+# SourceValue:1)”
+# {input_prompt}“how many ounces in 5.8 kilograms”{output_prompt}“UnitConvert(SourceUnit:kilogram,
+# TargetUnit:ounce, SourceValue:5.8)”
+# {input_prompt}“two to the power of 10”{output_prompt}“Calculate(2^10)”
+# {input_prompt}“2001-1989” {output_prompt}“Calculate(2001-1989)”
+# {input_prompt}“what is chatgpt”{output_prompt}“Search()”
+# {input_prompt}“primary year 1 maths calculation checklist”{output_prompt}“Search()”
+# {input_prompt}“what are different length units”{output_prompt}“Search()”
+# {input_prompt}“Natural logarithm of -3/18”{output_prompt}“Calculate(ln(-3/18))”
+# {input_prompt}“what is tan of 3/4”{output_prompt}“Calculate(tan(3/4))”
+
+
+## Add text variable to datasets 
+def create_text_datasets(example):
+    # Define your custom processing logic here
+    prompt_text = create_prompt_training(example['input'], example['output'])
+    return {"text": prompt_text}
+def process_dataset_dict(dataset_dict, processing_function):
+    processed_dict = DatasetDict()
+    for split_key, split_data in dataset_dict.items():
+        processed_data = split_data.map(processing_function)
+        processed_dict[split_key] = processed_data
+    return processed_dict
+# Concatenate "input" and "output" using the custom function
+q2f_datasets = process_dataset_dict(q2f_datasets, create_text_datasets)
+print(q2f_datasets)
+
+
+# Iterate over your training set and calculate the length of each sequence.
+# For example, you can use the following code:
+def get_max_seq_length(dataset):
+    max_seq_length = 0
+    for sequence in dataset['train']:
+        text_len = len(sequence['text'])
+        if text_len > max_seq_length:
+            max_seq_length = text_len
+    return max_seq_length+100
+max_seq_length = get_max_seq_length(q2f_datasets)
+
+
+### Prepare model for training
+# Freeze all parameters
+def freeze_all_parameters(model):
+    for param in model.parameters():
+        param.requires_grad = False
+    print(print_number_of_trainable_model_parameters(model))
+    return
